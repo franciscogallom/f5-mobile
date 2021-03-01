d4fe87e055820330ff7b514d30b3121f7d6b8c71
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-03-01 19:02:34 -0300

    feat: handle login.

--- screens/LogIn.tsx
+++ screens/LogIn.tsx
@@ -1,19 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 import { View, StyleSheet } from "react-native"
 
 import { Formik } from "formik"
+import Axios from "axios"
 
 import ButtonOne from "../components/ButtonOne"
 import InputLogInAndSignUp from "../components/InputLogInAndSignUp"
+import ErrorText from "../components/ErrorText"
+
+interface User {
+  user: string
+  password: string
+}
 
 const LogIn = (): JSX.Element => {
+  const [logInStatus, setLogInStatus] = useState("")
+
+  const handleLogIn = (values: User) => {
+    Axios.post("http://10.0.2.2:3001/login", values).then((response) => {
+      setLogInStatus(response.data.message || "successful login")
+    })
+  }
+
   return (
     <Formik
       initialValues={{
         user: "usuario.",
         password: "contraseña.",
       }}
-      onSubmit={(values) => alert(`${values.user} ${values.password}`)}
+      onSubmit={(values) => handleLogIn(values)}
     >
       {({ handleChange, handleSubmit, values }) => (
         <View style={logIn.container}>
@@ -27,6 +42,7 @@ const LogIn = (): JSX.Element => {
             isPassword={true}
             setDataType={handleChange("password")}
           />
+          {logInStatus ? <ErrorText text={logInStatus} /> : null}
           <ButtonOne text="iniciar sesión" handleTap={handleSubmit} />
         </View>
       )}
