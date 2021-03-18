57fad8d9a21b4885658dd446f1b402f72fbdbc96
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-03-18 00:27:45 -0300

    feat: starting with Home screen.

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,10 +1,25 @@
 import React, { FC } from "react"
 import { View, Text, StyleSheet } from "react-native"
+import { useSelector } from "react-redux"
+
+import { colors } from '../assets/colors'
+import { UserState } from "../redux/userReducer"
+
+import Search from '../components/Search'
+import BigCard from '../components/BigCard'
 
 const Home: FC = () => {
+
+  const user = useSelector<UserState, UserState["username"]>(
+    (state) => state.username
+  )
+
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text style={styles.greeting}>Hola {user}! Hoy se juega? ⚽</Text>
+      <Search />
+      <Text style={styles.text}>Nuestras mejores canchas ⭐.</Text>
+      <BigCard />
     </View>
   )
 }
@@ -12,9 +27,21 @@ const Home: FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: colors.primary,
+    padding: 20,
   },
+  greeting: {
+    color: colors.secondary,
+    paddingVertical: 40,
+    fontSize: 30,
+    fontFamily: 'poppins-extrabold'
+  },
+  text: {
+    color: colors.secondary,
+    paddingVertical: 40,
+    fontSize: 20,
+    fontFamily: 'poppins-bold-italic'
+  }
 })
 
 export default Home
