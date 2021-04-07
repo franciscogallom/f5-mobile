e2f444b8705a98c3e750389a86467779e33cba4c
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-04-07 11:16:49 -0300

    fix: props

--- services/getFields.ts
+++ services/getFields.ts
@@ -1,10 +1,11 @@
 import axios from "axios"
 import { HomeScreenNavigationProp } from "../screens/Home"
+import { FieldListScreenNavigationProp } from "../screens/FieldList"
 
 export const getFields = (
   setLoader: (bool: boolean) => void,
   setFields: (res: []) => void,
-  navigation: HomeScreenNavigationProp
+  navigation: HomeScreenNavigationProp | FieldListScreenNavigationProp
 ): void => {
   axios
     .get("http://10.0.2.2:3001/fields")
