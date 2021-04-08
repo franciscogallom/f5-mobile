cb50859b76ad25cd9635a9b7a061e05268297ff6
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-04-08 19:35:35 -0300

    feat: <FieldCard />.

---
+++ components/FieldCard.tsx
@@ -0,0 +1,67 @@
+import React, { FC } from "react"
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native"
+
+import { images } from "../components/Carousel"
+import { height } from "../assets/dimensions"
+import { colors } from "../assets/colors"
+import { Field } from "../screens/Home"
+
+interface Props {
+  field: Field
+  handleNavigation: () => void
+}
+
+const FieldCard: FC<Props> = ({ field, handleNavigation }: Props) => {
+  return (
+    <TouchableOpacity activeOpacity={0.9} onPress={handleNavigation}>
+      <View style={styles.card}>
+        <Text style={styles.name}>{field.name}.</Text>
+        <View style={styles.locationAndPriceContainer}>
+          <Text style={styles.detail}>📍 {field.location}.</Text>
+          <Text style={styles.detail}>💲{field.price}.</Text>
+        </View>
+        <Image source={images[field.id]} style={styles.image} />
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    marginVertical: height * 0.03,
+  },
+  locationAndPriceContainer: {
+    padding: 10,
+    position: "absolute",
+    width: "100%",
+    top: 125,
+    left: 0,
+    zIndex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  name: {
+    color: colors.secondary,
+    fontFamily: "poppins-extrabold-italic",
+    textTransform: "uppercase",
+    fontSize: 20,
+    position: "absolute",
+    top: -20,
+    left: 25,
+    zIndex: 1,
+    backgroundColor: colors.primary,
+    padding: 2.5,
+  },
+  detail: {
+    color: colors.secondary,
+    fontFamily: "poppins-extrabold",
+    fontSize: 16,
+    textAlign: "right",
+  },
+  image: {
+    height: height * 0.3,
+    width: "100%",
+    borderRadius: 20,
+  },
+})
+
+export default FieldCard
--- screens/FieldList.tsx
+++ screens/FieldList.tsx
@@ -1,22 +1,15 @@
 import React, { FC, useEffect, useState } from "react"
-import {
-  FlatList,
-  View,
-  TouchableOpacity,
-  Text,
-  StyleSheet,
-  Image,
-} from "react-native"
+import { FlatList, View, StyleSheet } from "react-native"
 import { StackNavigationProp } from "@react-navigation/stack"
 import { SharedElement } from "react-navigation-shared-element"
 
 import { getFields } from "../services/getFields"
 import { RootStackParamList, Field } from "./Home"
-import { images } from "../components/Carousel"
 import { height } from "../assets/dimensions"
 import { colors } from "../assets/colors"
 
 import Loader from "../components/Loader"
+import FieldCard from "../components/FieldCard"
 
 export type FieldListScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -42,24 +35,13 @@ const FieldList: FC<Props> = ({ navigation }: Props) => {
       <FlatList
         data={fields}
         keyExtractor={(item) => String(item.id)}
-        contentContainerStyle={{ padding: 10, marginTop: height * 0.05 }}
+        contentContainerStyle={{ padding: 10, marginVertical: height * 0.05 }}
         renderItem={({ item }) => {
           return (
-            <TouchableOpacity
-              activeOpacity={0.9}
-              onPress={() => {
-                navigation.navigate("FieldDetails", item)
-              }}
-            >
-              <View style={styles.card}>
-                <View style={{ padding: 10 }}>
-                  <Text style={styles.name}>{item.name}.</Text>
-                  <Text style={styles.detail}>📍 {item.location}.</Text>
-                  <Text style={styles.detail}>💲{item.price}.</Text>
-                </View>
-                <Image source={images[item.id]} style={styles.image} />
-              </View>
-            </TouchableOpacity>
+            <FieldCard
+              field={item}
+              handleNavigation={() => navigation.navigate("FieldDetails", item)}
+            />
           )
         }}
       />
@@ -67,47 +49,18 @@ const FieldList: FC<Props> = ({ navigation }: Props) => {
         id="details"
         style={[
           StyleSheet.absoluteFillObject,
-          {
-            transform: [{ translateY: height }],
-          },
+          { transform: [{ translateY: height }] },
         ]}
       >
         <View
-          style={[StyleSheet.absoluteFillObject, styles.detailsContainer]}
+          style={[
+            StyleSheet.absoluteFillObject,
+            { backgroundColor: "rgba(25, 20, 20, 0.75)" },
+          ]}
         />
       </SharedElement>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  card: {
-    height: height * 0.3,
-    borderWidth: 1,
-    borderColor: "#6d6b6b",
-    overflow: "hidden",
-    borderRadius: 20,
-    marginBottom: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  detail: {
-    color: colors.secondary,
-    fontFamily: "poppins-extrabold",
-  },
-  name: {
-    color: colors.secondary,
-    fontFamily: "poppins-extrabold-italic",
-    textTransform: "uppercase",
-    fontSize: 18,
-  },
-  image: {
-    height: "100%",
-    width: "40%",
-  },
-  detailsContainer: {
-    backgroundColor: "rgba(25, 20, 20, 0.75)",
-  },
-})
 
 export default FieldList
