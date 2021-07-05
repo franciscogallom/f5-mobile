9ea3e86c3cf6dba04c695fb8e71f3b89b88b952d
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-07-04 22:11:36 -0300

    improvement: better handling of bookings.

--- src/screens/Checkout.tsx
+++ src/screens/Checkout.tsx
@@ -1,21 +1,50 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 import { View, Text, StyleSheet, Button } from "react-native"
 import { RouteProp } from "@react-navigation/native"
 
 import { colors } from "../assets/colors"
 import { RootStackParamList } from "./Home"
 import { updateBookings } from "../services/updateBookings"
+import { HomeScreenNavigationProp } from "./Home"
+
+import Loader from "../components/Loader"
 
 type CheckoutScreenRouteProp = RouteProp<RootStackParamList, "Checkout">
 
 type Props = {
+  navigation: HomeScreenNavigationProp
   route: CheckoutScreenRouteProp
 }
 
-const Checkout: FC<Props> = ({ route }: Props) => {
+const Checkout: FC<Props> = ({ navigation, route }: Props) => {
   const { id, name, price, location, numberOfField, hour } = route.params
+  const [message, setMessage] = useState("")
+  const [loading, setLoading] = useState(false)
 
-  return (
+  function handlePress() {
+    setLoading(true)
+    updateBookings(id, numberOfField, hour)
+      .then(() => setMessage("Genial! En 5 segundos te redigiremos al Home."))
+      .catch(() =>
+        setMessage(
+          "Algo salió mal, o quizas alguien reservo justo antes que vos! Vuelve a intentarlo."
+        )
+      )
+      .finally(() => {
+        setLoading(false)
+        setTimeout(() => {
+          navigation.navigate("Home")
+        }, 5000)
+      })
+  }
+
+  return loading ? (
+    <Loader />
+  ) : message ? (
+    <View style={styles.container}>
+      <Text>{message}</Text>
+    </View>
+  ) : (
     <View style={styles.container}>
       <Text style={styles.title}>
         último paso! revisa que todo este bien antes de reservar tu cancha ⚠️.
@@ -26,11 +55,7 @@ const Checkout: FC<Props> = ({ route }: Props) => {
       <Text style={styles.text}>{location}.</Text>
       <Text style={styles.text}>${price}.</Text>
       {/* Will be replaced by swipe button */}
-      <Button
-        onPress={() => updateBookings(id, numberOfField, hour)}
-        title="CONFIRMAR"
-        color="#000000a2"
-      />
+      <Button onPress={handlePress} title="CONFIRMAR" color="#000000a2" />
     </View>
   )
 }
--- src/services/updateBookings.ts
+++ src/services/updateBookings.ts
@@ -1,11 +1,11 @@
-import axios from "axios"
+import axios, { AxiosPromise } from "axios"
 
 export const updateBookings = (
   id: string,
   numberOfField: string,
   hour: string
-): void => {
-  axios.put("http://10.0.2.2:3001/bookings/update", {
+): AxiosPromise => {
+  return axios.put("http://10.0.2.2:3001/bookings/update", {
     id,
     numberOfField,
     hour,
