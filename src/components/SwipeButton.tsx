0c011d3d35efac9f348bd91d5d12ab962da7870b
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-07-08 19:17:15 -0300

    feat: swipe button.

---
+++ src/components/SwipeButton.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from "react"
+import { SafeAreaView } from "react-native"
+import SwipeBtn from "rn-swipe-button"
+
+import { colors } from "../assets/colors"
+import { width } from "../assets/dimensions"
+
+type Props = {
+  handleSwipe: () => void
+}
+
+const swipeColor = "#A5FFD6"
+
+const SwipeButton: FC<Props> = ({ handleSwipe }: Props) => {
+  return (
+    <SafeAreaView>
+      <SwipeBtn
+        disabled={false}
+        swipeSuccessThreshold={width * 0.2}
+        height={50}
+        width={width * 0.8}
+        title="RESERVAR"
+        onSwipeSuccess={handleSwipe}
+        railFillBackgroundColor={swipeColor}
+        railFillBorderColor={swipeColor}
+        railBackgroundColor="rgba(25, 20, 20, 0.75)"
+        railBorderColor="rgba(25, 20, 20, 0.75)"
+        thumbIconBackgroundColor={swipeColor}
+        thumbIconBorderColor={swipeColor}
+        thumbIconImageSource={require("../assets/images/swipe-arrow.png")}
+        titleStyles={{
+          fontFamily: "poppins-extrabold-italic",
+          color: colors.secondary,
+        }}
+      />
+    </SafeAreaView>
+  )
+}
+
+export default SwipeButton
