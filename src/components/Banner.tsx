b1049e55f3ee104184f825c7a84d950e18887719
franciscogallom <franciscogallomolinuevo@gmail.com>
2022-02-17 15:36:24 -0300

    improvement: Banner shadow

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react"
 import { ImageBackground, Text, StyleSheet } from "react-native"
 import { TouchableOpacity } from "react-native-gesture-handler"
+import { Shadow } from "react-native-shadow-2"
 
 import { colors } from "../assets/colors"
 import { height } from "../assets/dimensions"
@@ -8,21 +9,28 @@ import { BannerProps } from "../interfaces/props"
 
 const Banner: FC<BannerProps> = ({ text, handleTap }: BannerProps) => {
   return (
-    <TouchableOpacity style={styles.container} onPress={handleTap}>
-      <ImageBackground
-        style={styles.image}
-        source={require("../assets/images/banner.jpg")}
-      >
-        <Text style={styles.text}>{text}</Text>
-      </ImageBackground>
-    </TouchableOpacity>
+    <Shadow
+      distance={8}
+      startColor={colors.shadow}
+      offset={[0, 5]}
+      viewStyle={{ width: "100%" }}
+      radius={40}
+    >
+      <TouchableOpacity style={styles.container} onPress={handleTap}>
+        <ImageBackground
+          style={styles.image}
+          source={require("../assets/images/banner.jpg")}
+        >
+          <Text style={styles.text}>{text}</Text>
+        </ImageBackground>
+      </TouchableOpacity>
+    </Shadow>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     height: height * 0.2,
-    width: "100%",
     borderRadius: 20,
     overflow: "hidden",
   },
