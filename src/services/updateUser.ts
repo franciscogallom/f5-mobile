599e6977e27ad1b653b81d5281cd1609187baf71
franciscogallom <franciscogallomolinuevo@gmail.com>
2022-02-23 20:58:46 -0300

    feat: add updateUser service

---
+++ src/services/updateUser.ts
@@ -0,0 +1,12 @@
+import axios from "axios"
+import { BASE_URL } from "../BASE_URL"
+import { UpdateUserResponse } from "../interfaces/interfaces"
+
+export const updateUser = async (
+  user: string,
+  attribute: string,
+  data: Record<string, unknown>
+): Promise<UpdateUserResponse> =>
+  axios
+    .put(`${BASE_URL}/users/update/${attribute}/${user}`, data)
+    .then((response) => response.data)
