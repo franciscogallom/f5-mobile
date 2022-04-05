aadd7f4f2c1a76c0f2dec5e868483810426063a6
franciscogallom <franciscogallomolinuevo@gmail.com>
2022-04-05 16:48:04 -0300

    fix: fonts path

--- src/config/getFonts.ts
+++ src/config/getFonts.ts
@@ -2,8 +2,8 @@ import * as Font from "expo-font"
 
 export const getFonts = (): Promise<void> =>
   Font.loadAsync({
-    "poppins-regular": require("./src/assets/fonts/Poppins-Regular.ttf"),
-    "poppins-bold-italic": require("./src/assets/fonts/Poppins-BoldItalic.ttf"),
-    "poppins-extrabold": require("./src/assets/fonts/Poppins-ExtraBold.ttf"),
-    "poppins-extrabold-italic": require("./src/assets/fonts/Poppins-ExtraBoldItalic.ttf"),
+    "poppins-regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "poppins-bold-italic": require("../assets/fonts/Poppins-BoldItalic.ttf"),
+    "poppins-extrabold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "poppins-extrabold-italic": require("../assets/fonts/Poppins-ExtraBoldItalic.ttf"),
   })
