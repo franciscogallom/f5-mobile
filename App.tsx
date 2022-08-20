eb6823a802ee87f254576df8cbffa68424d03094
franciscogallom <franciscogallomolinuevo@gmail.com>
2022-08-20 11:28:11 -0300

    improvement: better use of SplashScreen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import "react-native-gesture-handler"
-import React, { FC, useEffect, useState } from "react"
+import React, { FC, useEffect, useState, useCallback } from "react"
 import Toast from "react-native-toast-message"
 import * as SplashScreen from "expo-splash-screen/src/SplashScreen"
 import { Provider } from "react-redux"
@@ -11,25 +11,34 @@ import { getFonts } from "./src/config/getFonts"
 import { ContextProvider } from "./src/context/context"
 import { setTokenFromStorage } from "./src/asyncStorage/setTokenFromStorage"
 
+SplashScreen.preventAutoHideAsync()
+
 const App: FC = () => {
   const [appIsReady, setAppIsReady] = useState(false)
 
   useEffect(() => {
     async function prepare() {
       try {
-        await SplashScreen.preventAutoHideAsync()
         await getFonts()
         await setTokenFromStorage()
       } catch (e) {
         console.warn(e)
       } finally {
         setAppIsReady(true)
-        await SplashScreen.hideAsync()
       }
     }
-
     prepare()
   }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync()
+    }
+  }, [appIsReady])
+
+  useEffect(() => {
+    if (appIsReady) onLayoutRootView()
+  }, [appIsReady])
 
   return appIsReady ? (
     <Provider store={store}>
